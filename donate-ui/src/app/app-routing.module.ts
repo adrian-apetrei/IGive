209743da3e67317c2a4721bdc652fc7b0e94a149
narcisa minca <minca.narcisa@gmail.com>
2020-11-19 21:44:48 +0200

    fix

--- donate-ui/src/app/app-routing.module.ts
+++ donate-ui/src/app/app-routing.module.ts
@@ -1,46 +1,53 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
-import { AutoLoginGuard } from './guards/auto-login.guard';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./guards/auth.guard";
+import { AutoLoginGuard } from "./guards/auto-login.guard";
+// import { TabsPage } from "./pages/tabs/tabs.page";
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
-    canActivate: [AuthGuard]
+    path: "login",
+    loadChildren: () =>
+      import("./pages/login/login.module").then((m) => m.LoginPageModule),
+    canActivate: [AutoLoginGuard],
   },
   {
-    path: '',
-    redirectTo: 'register',
-    pathMatch: 'full',
-    canActivate: [AutoLoginGuard]
+    path: "register",
+    loadChildren: () =>
+      import("./pages/register/register.module").then(
+        (m) => m.RegisterPageModule
+      ),
+    canActivate: [AutoLoginGuard],
   },
   {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
-    canActivate: [AutoLoginGuard]
+    path: "user-preferences",
+    loadChildren: () =>
+      import("./pages/user-preferences/user-preferences.module").then(
+        (m) => m.UserPreferencesPageModule
+      ),
+    canActivate: [AuthGuard],
   },
   {
-    path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule),
-    canActivate: [AutoLoginGuard]
+    path: "payment",
+    loadChildren: () =>
+      import("./pages/payment/payment.module").then((m) => m.PaymentPageModule),
   },
   {
-    path: 'user-preferences',
-    loadChildren: () => import('./pages/user-preferences/user-preferences.module').then( m => m.UserPreferencesPageModule),
-    canActivate: [AuthGuard]
+    path: "",
+    redirectTo: "register",
+    pathMatch: "full",
+    canActivate: [AutoLoginGuard],
   },
   {
-    path: 'payment',
-    loadChildren: () => import('./pages/payment/payment.module').then( m => m.PaymentPageModule)
+    path: "tabs",
+    loadChildren: "./pages/tabs/tabs.module#TabsPageModule",
   },
-
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- donate-ui/src/app/guards/auto-login.guard.ts
+++ donate-ui/src/app/guards/auto-login.guard.ts
@@ -1,24 +1,23 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
-import { AuthService } from '../services/auth.service';
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { Observable } from "rxjs";
+import { map, take } from "rxjs/operators";
+import { AuthService } from "../services/auth.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AutoLoginGuard implements CanActivate {
-
   constructor(private auth: AuthService, private router: Router) {}
 
   canActivate(): Observable<boolean> {
     return this.auth.user.pipe(
       take(1),
-      map(user => {
+      map((user) => {
         if (!user) {
           return true;
         } else {
-          this.router.navigateByUrl('/home');
+          this.router.navigateByUrl("/tabs/home");
           return false;
         }
       })

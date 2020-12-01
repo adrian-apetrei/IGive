d684ed21cafc17985f97e52b769fedaf16389804
adrian-apetrei <adrian.apetrei.89@gmail.com>
2020-12-01 12:18:42 +0200

    register custom icons

--- donate-ui/src/app/app.component.ts
+++ donate-ui/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +15,15 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
   ) {
     this.initializeApp();
+    this.matIconRegistry.addSvgIcon(
+      "svg-checkmark",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/checkmark.svg")
+    );
   }
 
   initializeApp() {

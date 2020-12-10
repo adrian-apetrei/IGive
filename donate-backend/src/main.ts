c46ca99a448b080698212a90999b79b1ce73b455
narcisa minca <minca.narcisa@gmail.com>
2020-12-10 18:38:32 +0200

    test

--- donate-backend/src/main.ts
+++ donate-backend/src/main.ts
@@ -6,7 +6,13 @@ import { ConfigReader } from 'neconfig';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
-    cors: true,
+    cors: {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    },
   });
 
   app.enableCors();

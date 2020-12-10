ff63f96b46d85111760e37f4fef4800a2371af33
narcisa minca <minca.narcisa@gmail.com>
2020-12-10 18:15:48 +0200

    payment

--- donate-backend/src/payment/payment.module.ts
+++ donate-backend/src/payment/payment.module.ts
@@ -1,6 +1,8 @@
 import { HttpModule, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from 'src/config/config.module';
 import { ConfigService } from 'src/config/config.service';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
@@ -12,6 +14,13 @@ import { PaymentMethodsSchema } from './schemas/payment.schema';
       { name: 'Payment-Method', schema: PaymentMethodsSchema },
     ]),
     PassportModule.register({ defaultStrategy: 'jwt', session: false }),
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+      }),
+      imports: [ConfigModule],
+      inject: [ConfigService],
+    }),
     HttpModule,
   ],
   providers: [PaymentService, ConfigService],
